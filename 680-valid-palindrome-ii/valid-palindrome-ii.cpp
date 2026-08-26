384e3817892f197c7d0f9d91137f7fe75ede5a39
Sagar Singh <sagardosad2007@gmail.com>
2026-08-26 23:16:43 +0530

    Time: 12 ms (6.70%) | Memory: 26.6 MB (24.21%) - LeetSync

---
+++ 680-valid-palindrome-ii/valid-palindrome-ii.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+
+    bool isPalindrome(string s, int low, int high) {
+
+        while (low < high) {
+
+            if (s[low] != s[high]) {
+                return false;
+            }
+
+            low++;
+            high--;
+        }
+
+        return true;
+    }
+
+    bool validPalindrome(string s) {
+
+        int low = 0;
+        int high = s.length() - 1;
+
+        while (low < high) {
+
+            if (s[low] == s[high]) {
+                low++;
+                high--;
+            }
+
+            else {
+                return isPalindrome(s, low + 1, high) ||
+                       isPalindrome(s, low, high - 1);
+            }
+        }
+
+        return true;
+    }
+};
