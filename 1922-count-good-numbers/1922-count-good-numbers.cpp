7264ca0fe3e0ffae0ca9dd964374fdc47a274cdd
Sagar <sagardosad2007@gmail.com>
2026-09-08 11:07:28 +0530

    Time: 0 ms (100%), Space: 7.8 MB (71.29%) - LeetHub

---
+++ 1922-count-good-numbers/1922-count-good-numbers.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+const int M=1e9+7;
+    long long pow(long long a,long long b){
+        if (b==0) return 1;
+        long long half=pow(a,b/2);
+        long long result=(half*half)%M;
+        if(b%2==0){
+            return result;
+        }
+        else{
+            return (result*a)%M;
+        }
+    }
+    int countGoodNumbers(long long n) {
+        return (long long) pow(5,(n+1)/2) * pow(4,n/2)%M;
+    }
+};
